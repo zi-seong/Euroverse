c4bfc0b383c5e0066bcaa5ee3630f57290c92c30
CCCS <CCCS@DESKTOP-D082UJB>
2020-03-23 16:56:44 +0900

    JS (2020/03/23 월요일) bit TeamProject File
    Euroverse

---
+++ euroverse/src/com/ksy/common/Config.java
@@ -0,0 +1,10 @@
+package com.ksy.common;
+
+public final class Config {
+	
+	public static final String appid = "청기와랩 아이디";
+	public static final String apikey = "청기와랩apikey";
+	public static final String content = "EUROVERSE 핸드폰 인증 서비스 \n 인증번호는:";
+	public static final String sender = "보내는 번호";
+
+}
